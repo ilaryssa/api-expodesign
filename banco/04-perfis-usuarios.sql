3073bdbef2405a19bec9dfff53ff4fa8d85787d2
Laryssa Santos <laryssasantos@alu.ufc.br>
2026-06-28 23:55:34 -0300

    fix(banco): Ajustes nos scripts SQL

--- banco/04-perfis-usuarios.sql
+++ banco/04-perfis-usuarios.sql
@@ -1,9 +1,9 @@
 	-- Criação de Usuários e Controle de Acesso
 
 --Aqui eu crio os três roles principais
-create role administrador
-create role autor
-create role leitor
+create role administrador;
+create role autor;
+create role leitor;
 
 --Dou as autorizações descritas acima para cada role
 grant select on all tables in schema public
@@ -17,11 +17,11 @@ grant all privileges on
 to administrador;
 
 --Aqui eu crio 3 usuários, cada um com um role diferente.
-create user daniely_admin with password 'senhai123'
+create user daniely_admin with password 'senhai123';
 grant administrador to daniely_admin;
 
-create user lara_autora with password 'senha456'
+create user lara_autora with password 'senha456';
 grant autor to lara_autora;
 
-create user laryssa_leitora with password 'senha789'
-grant leitor to laryssa_leitora
+create user laryssa_leitora with password 'senha789';
+grant leitor to laryssa_leitora;
