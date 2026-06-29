5f76ee138e9f5def43819756663950a780fdad33
Daniely M. Maia <daniely.maia@alu.ufc.br>
2026-06-29 13:23:01 -0300

    refactor(sql): refactor user and role creation SQL script

--- banco/04-perfis-usuarios.sql
+++ banco/04-perfis-usuarios.sql
@@ -1,11 +1,11 @@
 	-- Criação de Usuários e Controle de Acesso
 
---Aqui eu crio os três roles principais
+-- Criação dos 3 principais roles
 create role administrador;
 create role autor;
 create role leitor;
 
---Dou as autorizações descritas acima para cada role
+-- Provendo as autorizações para cada role
 grant select on all tables in schema public
 to leitor;
 
@@ -16,8 +16,8 @@ grant all privileges on
 	usuario, autor, projeto, galeria, disciplina, trabalho_em_equipe, membros_equipe
 to administrador;
 
---Aqui eu crio 3 usuários, cada um com um role diferente.
-create user daniely_admin with password 'senhai123';
+-- Criação de 3 usuários, um para cada role
+create user daniely_admin with password 'senha123';
 grant administrador to daniely_admin;
 
 create user lara_autora with password 'senha456';
